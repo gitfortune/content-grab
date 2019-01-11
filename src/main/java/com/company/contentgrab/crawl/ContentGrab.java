842105ec2c55f780fc62662f26be9348029ba762
zheng319 <zheng319.happy@163.com>
2019-01-11 18:11:51 +0800

    内容抓取v0.01

--- src/main/java/com/company/contentgrab/crawl/ContentGrab.java
+++ src/main/java/com/company/contentgrab/crawl/ContentGrab.java
@@ -5,6 +5,7 @@ import com.company.contentgrab.common.RestResponse;
 import com.company.contentgrab.enmu.ResultEnmu;
 import com.company.contentgrab.entity.ArticleDTO;
 import com.company.contentgrab.exception.GrabException;
+import com.company.contentgrab.utils.DateUtil;
 import com.gargoylesoftware.htmlunit.BrowserVersion;
 import com.gargoylesoftware.htmlunit.WebClient;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
@@ -46,34 +47,82 @@ public class ContentGrab {
     public void process(){
 //        grabCNRLinks(cnr);
 //        grabSinaLinks(sina);
+//        grabDaHeLinks(dahe);
+        LocalDate parse = LocalDate.parse("2019-12-03");
+        log.info(parse.toString());
     }
 
     /**
-     * 抓取大河网新闻
+     * 抓取大河网新闻列表链接
      */
     public void grabDaHeLinks(String url){
-        int i = 0;
         try {
             Document doc = Jsoup.connect(url).get();
             //根据页面html的结构，获取该页面的新闻列表里的url
-            Element content = doc.getElementById("articleList");
-            Elements links = content.getElementsByTag("a");
-
-
-            for (Element link : links) {
-                if(i > 10){
-                    break;
+            //获取新闻列表 li标签集合
+            Elements lis = doc.select("#content li");
+            for (Element li : lis) {
+                String linkHref = li.getElementsByTag("a").attr("href");
+                String linkText = li.getElementsByTag("a").text();
+                //截取年月日 时间
+                String time = li.getElementsByTag("span").text().substring(0, 10);
+                if(DateUtil.isToday(time)){
+                    //是当天新闻，继续解析
+                    this.parseDaheNewsHtml(linkHref);
                 }
-                String linkHref = link.attr("href");
-//                this.saveNews(linkHref);
-                String linkText = link.text();
-                log.info(linkHref+""+linkText+ i++);
+//                log.info(linkHref+""+linkText+ ""+time);
             }
         } catch (IOException e) {
-            log.error("JSOUP获取HTML时发生错误：{}",e.getMessage());
+            log.error("JSOUP获取大河网新闻列表时发生异常：{}",e.getMessage());
             throw new GrabException(ResultEnmu.JSOUP_FAIL);
         }
     }
+
+    /**
+     * 解析大河网新闻文章页面
+     */
+    public void parseDaheNewsHtml(String url){
+        try {
+            ArticleDTO articleDTO = new ArticleDTO();
+            Connection conn = Jsoup.connect(url).timeout(5000);
+            conn.header("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8");
+            conn.header("Accept-Encoding", "gzip, deflate, sdch");
+            conn.header("Accept-Language", "zh-CN,zh;q=0.8");
+            conn.header("User-Agent", "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36");
+
+            Document doc = conn.get();
+            log.info("标题："+doc.getElementById("4g_title").text());
+            Element pubtime = doc.getElementById("pubtime_baidu");
+            log.info("发布时间"+pubtime.text());
+            Element source = doc.getElementById("source_baidu");
+            log.info("来源"+source.text());
+            Element body = doc.getElementById("mainCon");
+            Elements imgs = body.getElementsByTag("img");
+            for (Element img : imgs){
+                log.info("图片："+img.attr("src"));
+            }
+            log.info("内容"+body.html());
+            Element editor = doc.getElementById("editor_baidu");
+            log.info("编辑"+editor.text());
+            //标题
+            articleDTO.setArticleTitle(doc.getElementById("4g_title").text());
+            articleDTO.setContentTitle(doc.getElementById("4g_title").text());
+            //发布时间
+            articleDTO.setPublishTime(doc.getElementById("pubtime_baidu").text());
+            //来源
+            articleDTO.setArticleOrigin(doc.getElementById("source_baidu").text());
+            //内容
+            articleDTO.setContentBody(doc.getElementById("mainCon").html());
+            //作者
+            articleDTO.setArticleAuthor(doc.getElementById("editor_baidu").text());
+            RestResponse<ArticleDTO> articleDTORestResponse = articleServiceAPI.create("-1", "-1", articleDTO);
+            log.info("状态码："+articleDTORestResponse.getCode());
+        } catch (IOException e) {
+            log.error("JSOUP解析大河网文章时发生异常：{}",e.getMessage());
+            throw new GrabException(ResultEnmu.JSOUP_FAIL);
+        }
+    }
+
 
     /**
      * 抓取央广新闻链接
@@ -83,15 +132,14 @@ public class ContentGrab {
         try {
             Document doc = Jsoup.connect(url).get();
             //根据页面html的结构，获取该页面的新闻列表里的url
-            Elements links = doc.select(".articleList .text a");
-            for (Element link : links) {
-                if(i > 10){
-                    break;
-                }
-                String linkHref = link.attr("href");
+            //获取新闻列表 li标签集合
+            Elements lis = doc.select(".articleList li");
+            for (Element li : lis) {
+                String linkHref = li.select(".text a").attr("href");
 //                this.saveNews(linkHref);
-                String linkText = link.text();
-                log.info(linkHref+""+linkText+ i++);
+                String linkText = li.select(".text a").text();
+                String time = li.select(".publishTime").text().substring(0, 10);    //时间
+                log.info(linkHref+""+linkText+ ""+time);
             }
         } catch (IOException e) {
             log.error("JSOUP获取HTML时发生错误：{}",e.getMessage());
@@ -145,45 +193,7 @@ public class ContentGrab {
      */
     public void saveNews(String url){
         String url1 = "https://news.dahe.cn/2018/09-16/376490.html";
-        try {
-            ArticleDTO articleDTO = new ArticleDTO();
-            Connection conn = Jsoup.connect(url1).timeout(5000);
-            conn.header("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8");
-            conn.header("Accept-Encoding", "gzip, deflate, sdch");
-            conn.header("Accept-Language", "zh-CN,zh;q=0.8");
-            conn.header("User-Agent", "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36");
 
-            Document doc = conn.get();
-            log.info("标题："+doc.getElementById("4g_title").text());
-            Element pubtime = doc.getElementById("pubtime_baidu");
-            log.info("发布时间"+pubtime.text());
-            Element source = doc.getElementById("source_baidu");
-            log.info("来源"+source.text());
-            Element body = doc.getElementById("mainCon");
-            Elements imgs = body.getElementsByTag("img");
-            for (Element img : imgs){
-                log.info("图片："+img.attr("src"));
-            }
-            log.info("内容"+body.html());
-            Element editor = doc.getElementById("editor_baidu");
-            log.info("编辑"+editor.text());
-            //标题
-            articleDTO.setArticleTitle(doc.getElementById("4g_title").text());
-            articleDTO.setContentTitle(doc.getElementById("4g_title").text());
-            //发布时间
-            articleDTO.setPublishTime(doc.getElementById("pubtime_baidu").text());
-            //来源
-            articleDTO.setArticleOrigin(doc.getElementById("source_baidu").text());
-            //内容
-            articleDTO.setContentBody(doc.getElementById("mainCon").html());
-            //作者
-            articleDTO.setArticleAuthor(doc.getElementById("editor_baidu").text());
-            RestResponse<ArticleDTO> articleDTORestResponse = articleServiceAPI.create("-1", "-1", articleDTO);
-            log.info("状态码："+articleDTORestResponse.getCode());
-        } catch (IOException e) {
-            log.error("JSOUP获取HTML时发生异常：{}",e.getMessage());
-            throw new GrabException(ResultEnmu.JSOUP_FAIL);
-        }
     }
 
 }
---
+++ src/main/java/com/company/contentgrab/utils/DateUtil.java
@@ -0,0 +1,20 @@
+package com.company.contentgrab.utils;
+
+import java.time.LocalDate;
+
+public class DateUtil {
+
+    /**
+     *  判断是否是当天
+     * @param str 格式为xxxx-xx-xx的时间字符串
+     * @return
+     */
+    public static boolean isToday(String str){
+        LocalDate lld1 = LocalDate.now();
+        LocalDate lld2 = LocalDate.parse(str);
+        if (lld1.equals(lld2)) {
+            return true;
+        }
+        return false;
+    }
+}
